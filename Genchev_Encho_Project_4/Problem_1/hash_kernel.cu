e156dd481381a085176cfb0d61c6f7c4fcf8ce42
EnchoGenchev <esgenchev@gmail.com>
2026-03-23 18:48:31 -0500

    problem 1 works

--- Genchev_Encho_Project_4/Problem_1/hash_kernel.cu
+++ Genchev_Encho_Project_4/Problem_1/hash_kernel.cu
@@ -1,13 +1,22 @@
+#define MAX 123123123
+__global__ void hash_kernel(
+    unsigned int* nonce_array,
+    unsigned int* transactions,
+    unsigned int* hash_array,
+    unsigned int n_transactions,
+    unsigned int trials
+) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-/* Hash Kernel --------------------------------------
-*       Generates an array of hash values from nonces.
-*/
-__global__
-void hash_kernel(unsigned int* hash_array, unsigned int* nonce_array, unsigned int array_size, unsigned int* transactions, unsigned int n_transactions, unsigned int mod) {
+    if (i < trials) {
+        unsigned int nonce = nonce_array[i];
 
-    // Calculate thread index
-    unsigned int index = blockDim.x * blockIdx.x + threadIdx.x;
+        unsigned int hash = (nonce + transactions[0] * (i + 1)) % MAX;
 
-    // TODO: Generate hash values
+        for (int j = 1; j < n_transactions; j++) {
+            hash = (hash + transactions[j] * (i + 1)) % MAX;
+        }
 
-} // End Hash Kernel //
+        hash_array[i] = hash;
+    }
+}
